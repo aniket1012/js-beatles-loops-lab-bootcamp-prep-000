b27278c83f55e63dc59d682da4f9a0a70d67f7a1
Aniket Patel <aniketupatel1@gmail.com>
2019-02-17 00:19:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments) {
   var array = [];
   for ( let i = 0; i < i.length, i++) {
-    array.push()
+    array.push(musicians[i] + "plays" + instruments[i]);
   }
 }
