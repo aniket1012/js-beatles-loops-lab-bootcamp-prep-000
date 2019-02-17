99ebc61cd4f8e52a137da31d4c51bf9d781b23fc
Aniket Patel <aniketupatel1@gmail.com>
2019-02-17 00:27:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ function theBeatlesPlay(musicians,instruments) {
 }
 
 function johnLennonFacts() {
-  const facts = [
+  var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
 
-const newFacts = [];
-
+var newFacts = [];
+var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
+}
 }
